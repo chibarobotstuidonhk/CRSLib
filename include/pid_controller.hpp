86a5e8ae8bc07a0bb6b2e49860b8d1d33344f2ff
Stew-000-1-0-011 <92617876+Stew-000-1-0-011@users.noreply.github.com>
2022-10-18 18:49:33 +0900

    pid制御用のコードを追加

---
+++ include/pid_controller.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <type_traits>
+#include <CRSLib/include/std_int.hpp>
+
+namespace CRSLib
+{
+	template<class T, size_t look_past_size>
+	requires std::is_arithmetic_v<T>
+	struct PidController final
+	{
+		T gain_p;
+		T gain_i;
+		T gain_d;
+
+		T past_error_total{};
+		T last_error{};
+
+		T calculate(const T current_error) const noexcept
+		{
+			return gain_p * current_error + gain_i * past_error_total + gain_d * (current_error - last_error);
+		}
+
+		void update(const T current_error) noexcept
+		{
+			past_error_total = (past_error_total * (look_past_size - 1) + current_error) / look_past_size;
+			last_error = past_error;
+		}
+	};
+}
